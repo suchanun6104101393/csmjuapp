8950489d26e6e62bacb8624eaaf2fb65bfa981d9
suchanun6104101393 <suchanun0209@gmail.com>
2021-11-21 14:14:45 +0700

    1

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:csmjuapp/components/carasousel_silder.dart';
 import 'package:csmjuapp/components/carosel_loading.dart';
 import 'package:csmjuapp/controller/home_controller.dart';
 import 'package:flutter/material.dart';
@@ -13,19 +14,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: GetBuilder<HomeController>(
-            builder: (_c){
-              if(_c.isLoading)
-                return CarouselLoading();
+        body: SafeArea(child: GetBuilder<HomeController>(
+            builder: (_c) {
+              if (_c.isLoading) if (_c.carouselData.length > 0)
+                return CarouselSliderDataFound(_c.carouselData);
               else
-                return Container();
-               
+                return CarouselLoading();
+              else if (_c.carouselData.length > 0)
+                return CarouselSliderDataFound(_c.carouselData);
+              else
+                return Container(
+                  
+                );
             },
-          )
-      ),
-      
-    );
+          ),
+          
+        )
+        );
+    
     
   }
 }
